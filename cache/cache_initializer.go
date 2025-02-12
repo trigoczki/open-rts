0031202af5f9e56c494ca9cba39470b094a57745
Tamas Rigoczki <rigoczkitamas@gmail.com>
2025-02-12 21:15:16 +0100

    feat: Add resource caching and endpoint for retrieving resources

--- cache/cache_initializer.go
+++ cache/cache_initializer.go
@@ -2,7 +2,7 @@ package cache
 
 import (
 	"log"
-	"open-rts/ruleset"
+	"open-rts/ruleset/building"
 	rulesetResource "open-rts/ruleset/resource"
 )
 
@@ -12,14 +12,15 @@ func Initialize() {
 		panic(err)
 	}
 
-	err = ruleset.ValidateJSON(resources, "ruleset/resource/resource_schema.json")
+	buildings, err := building.ParseBuildings("ruleset/building/buildings_ruleset.json")
 	if err != nil {
 		panic(err)
 	}
-	log.Println("Resources validated successfully")
 
 	cache := GetInstance()
 	cache.SetResources(resources)
 	log.Println("Resources set in cache")
+	cache.SetBuildings(buildings)
+	log.Println("Buildings set in cache")
 	log.Println("Cache initialized successfully")
 }
